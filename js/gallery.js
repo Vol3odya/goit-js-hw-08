add610969011995f6d07606b57e6eedf8ff9af6a
Vol3odya <Vol_odya@ukr.net>
2024-06-23 15:37:18 +0300

    finish

--- js/gallery.js
+++ js/gallery.js
@@ -1,30 +1,89 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
+const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
   },
-  getDiscount() {
-    return this.discount;
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
   },
-  setDiscount(value) {
-    this.discount = value;
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
   },
-  getOrders() {
-    return this.orders;
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
   },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
   },
-  // Change code above this line
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+const head = document.querySelector('head');
+head.insertAdjacentHTML("beforeend", '<link href=" https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css " rel="stylesheet">');
+const body = document.querySelector('body');
+let str = '<ul class="gallery">';
+images.forEach((elm) => {
+  str += '<li class="gallery-item"> <a class="gallery-link" href=' + elm.original + '> <img class="gallery-image" src=' + elm.preview + ' data-source=' + elm.original + ' alt=' + elm.description + '/> </a></li> ';
+});
+str += '</ul> <script src=" https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js "></script> <link href=" https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css " rel="stylesheet">';
+body.insertAdjacentHTML("afterbegin", str);
+const ul = document.querySelector('ul');
+ul.addEventListener('click', hundlerProdukt);
+function hundlerProdukt(evt){
+  if (!evt.target.classList.contains("gallery-image")) { 
+    return;
+  }
+  //const rez = evt.target.closest('.gallery-image');
+  const rez = evt.target.dataset.source;
+  //console.log('sddvds');
+  console.log('Data-source', rez);
+  evt.preventDefault();
+  const instance = basicLightbox.create('<img src="https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg" />');
+  instance.show()
+  //basicLightbox.create('<img src="${evt.target.dataset.source}" />').show();
 };
-
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
